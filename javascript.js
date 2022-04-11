919e23b3e885fa8d0afcc821ff33bbaab91b7546
Brandonn Melchor <97859992+brandonnmelchor@users.noreply.github.com>
2022-04-11 15:10:32 -0500

    create new functions. needs testing and update

--- javascript.js
+++ javascript.js
@@ -1,9 +1,50 @@
+// Operations
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+const modulus = (a, b) => a % b;
+
+const operator = (a, b) => {
+  add(a, b);
+};
+
+// Selectors and Event Listeners
+
 let calculation = document.querySelector(".calculation");
 let result = document.querySelector(".result");
 
-const caculatorKeys = document.querySelectorAll("div:not(.display, .calculator)");
-caculatorKeys.forEach((key) => key.addEventListener("click", test));
+const numbers = document.querySelectorAll(".number");
+numbers.forEach((number) => number.addEventListener("click", inputNumber));
 
-function test() {
+const operations = document.querySelectorAll(".operation");
+operations.forEach((operation) => operation.addEventListener("click", inputOperation));
+
+const calculate = document.querySelector(".calculate");
+calculate.addEventListener("click", calculateInputs);
+
+//
+
+let currentNumber = "";
+let values = [];
+
+function inputNumber() {
   calculation.textContent += this.textContent;
+  currentNumber += this.textContent;
+}
+
+function inputOperation() {
+  calculation.textContent += ` ${this.textContent} `;
+
+  values.push(currentNumber);
+  currentNumber = "";
+
+  values.push(this.attributes["id"].nodeValue);
+}
+
+function calculateInputs() {
+  values.push(currentNumber);
+  result.textContent = "DONE";
+  console.log(values);
 }
